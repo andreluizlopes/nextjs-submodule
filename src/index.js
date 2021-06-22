2008e70cb0e7870ed740230725e5015478d6f0b6
André Lopes <lopes.andree@gmail.com>
2021-06-22 10:27:19 -0300

    add theme module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+let theme = () => console.log('default theme')
+
+try {
+  theme = require('../../modules/theme')
+} catch (e) {
+  console.log('theme module not found')
+}
+
 function main () {
+  theme()
   console.log('main')
 }
 
